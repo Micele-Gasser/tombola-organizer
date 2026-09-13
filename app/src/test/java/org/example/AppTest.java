81b1e09b6acc87d2f5980627f2b2df96ac92dc8a
Michel Gasser <gassemi5@students.zhaw.ch>
2026-09-13 21:32:19 +0200

    changed file hierarchy and added existing files

--- app/src/test/java/org/example/AppTest.java
+++ app/src/test/java/org/example/AppTest.java
@@ -4,10 +4,14 @@
 package org.example;
 
 import org.junit.jupiter.api.Test;
+
+import App;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class AppTest {
-    @Test void appHasAGreeting() {
+    @Test
+    void appHasAGreeting() {
         App classUnderTest = new App();
         assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
     }
